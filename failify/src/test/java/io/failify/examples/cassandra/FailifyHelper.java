ca0948ec146f25d71d65c932953e19b9b5d797f6
Armin Balalaie <armin.balalaie@gmail.com>
2019-05-20 17:29:27 -0700

    adds placeholder config file and better detection of cluster startup. adds configs to make the cluster more efficient.

--- failify/src/test/java/io/failify/examples/cassandra/FailifyHelper.java
+++ failify/src/test/java/io/failify/examples/cassandra/FailifyHelper.java
@@ -1,57 +1,55 @@
 package io.failify.examples.cassandra;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import io.failify.FailifyRunner;
 import io.failify.dsl.entities.Deployment;
 import io.failify.dsl.entities.PathAttr;
-import io.failify.dsl.entities.PortType;
 import io.failify.exceptions.RuntimeEngineException;
-import io.failify.execution.CommandResults;
+import io.failify.execution.ULimit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.StringJoiner;
-import java.util.concurrent.TimeoutException;
 
 public class FailifyHelper {
     public static final Logger logger = LoggerFactory.getLogger(FailifyHelper.class);
 
     public static Deployment getDeployment(int numOfNodes) {
-        String version = "3.11.4-SNAPSHOT";
+        String version = "3.11.4-SNAPSHOT"; // this can be dynamically generated from maven metadata
         String dir = "apache-cassandra-" + version;
+        StringJoiner seeds = new StringJoiner(",");
+        for (int i=1; i<=numOfNodes; i++) seeds.add("n" + i);
         Deployment.Builder builder = Deployment.builder("example-cassandra")
             .withService("cassandra")
                 .applicationPath("../cassandra-3.11.4-build/" + dir + "-bin.tar.gz", "/cassandra", PathAttr.COMPRESSED)
-                .startCommand("/cassandra/" + dir + "/bin/cassandra -R")
+                .workDir("/cassandra/" + dir).startCommand("bin/cassandra -R")
                 .dockerImageName("failify/cassandra:1.0").dockerFileAddress("docker/Dockerfile", true)
                 .logDirectory("/cassandra/" + dir + "/logs")
-                .applicationPath("config/cassandra.yaml", "/cassandra/" + dir + "/conf/cassandra.yaml")
-                .tcpPort(9160).and();
+                .applicationPath("config/cassandra.yaml", "/cassandra/" + dir + "/conf/cassandra.yaml",
+                        new HashMap<String, String>() {{ put("CASSANDRA_SEEDS", seeds.toString());}})
+                .applicationPath("config/cassandra-env.sh", "/cassandra/" + dir + "/conf/cassandra-env.sh")
+                .ulimit(ULimit.MEMLOCK, -1).and();
         for (int i=1; i<=numOfNodes; i++) builder.withNode("n" + i, "cassandra").and();
         return builder.build();
     }
 
-    public static CqlSession getCQLSession(FailifyRunner runner) {
-        return CqlSession.builder().addContactPoint(new InetSocketAddress("n1", 9042))
-                .withLocalDatacenter("dc1").build();
+    public static CqlSession getCQLSession(FailifyRunner runner, int numOfContactPoints) {
+        CqlSessionBuilder builder = CqlSession.builder().withLocalDatacenter("dc1");
+        for (int i=1; i<=numOfContactPoints; i++) builder.addContactPoint(new InetSocketAddress("n" + i, 9042));
+        return builder.build();
     }
 
-    public static void waitForCluster(FailifyRunner runner) throws RuntimeEngineException {
+    public static void waitForCluster(FailifyRunner runner, int numOfNodes) throws RuntimeEngineException {
         logger.info("Waiting for cluster to start up ...");
-        String version = "3.11.4-SNAPSHOT";
-        String dir = "apache-cassandra-" + version;
-        CommandResults results;
+
+
+        boolean started = false;
         int attempt = 0;
         do {
-            if (attempt++ >= 2) {
+            if (attempt++ >= 15) {
                 throw new RuntimeEngineException("Timeout in waiting for cluster startup");
             }
             try {
@@ -59,8 +57,13 @@ public class FailifyHelper {
             } catch (InterruptedException e) {
                 throw new RuntimeEngineException("waitForCluster sleep thread got interrupted");
             }
-            results = runner.runtime().runCommandInNode("n1", "/cassandra/" + dir + "/bin/nodetool status");
-            logger.info("nodetool results:\n{}", results.stdErr()+ results.stdOut());
-        } while (results.exitCode() != 0);
+
+            try {
+                getCQLSession(runner, numOfNodes);
+                started = true;
+            } catch (Exception e) {
+                // possible place to debug log something
+            }
+        } while (!started);
     }
 }
--- failify/src/test/java/io/failify/examples/cassandra/SampleTest.java
+++ failify/src/test/java/io/failify/examples/cassandra/SampleTest.java
@@ -4,9 +4,7 @@ package io.failify.examples.cassandra;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.*;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.datastax.oss.driver.api.core.metadata.EndPoint;
 import io.failify.FailifyRunner;
-import io.failify.dsl.entities.Deployment;
 import io.failify.exceptions.RuntimeEngineException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -15,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static org.junit.Assert.*;
 
-import java.net.InetSocketAddress;
 import java.sql.*;
 import java.util.concurrent.TimeoutException;
 
@@ -23,12 +20,12 @@ public class SampleTest {
     private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
 
     protected static FailifyRunner runner;
+    protected static final int NUM_OF_NODES = 3;
 
     @BeforeClass
     public static void before() throws RuntimeEngineException, TimeoutException {
-        Deployment deployment = FailifyHelper.getDeployment(3);
-        runner = FailifyRunner.run(deployment);
-        FailifyHelper.waitForCluster(runner);
+        runner = FailifyHelper.getDeployment(NUM_OF_NODES).start();
+        FailifyHelper.waitForCluster(runner, NUM_OF_NODES);
         logger.info("The cluster is UP!");
     }
 
@@ -42,10 +39,10 @@ public class SampleTest {
 
     @Test
     public void sampleTest() throws RuntimeEngineException, SQLException, ClassNotFoundException, TimeoutException {
-        try (CqlSession session = FailifyHelper.getCQLSession(runner)) {                                  // (1)
-            ResultSet rs = session.execute("select release_version from system.local");              // (2)
+        try (CqlSession session = FailifyHelper.getCQLSession(runner, NUM_OF_NODES)) {
+            ResultSet rs = session.execute("select release_version from system.local");
             Row row = rs.one();
-            System.out.println(row.getString("release_version"));                                    // (3)
+            System.out.println(row.getString("release_version"));
         }
     }
 }
